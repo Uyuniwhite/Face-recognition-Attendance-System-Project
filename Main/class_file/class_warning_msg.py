e79a1145549995d21e8785bca3402f4c95d894ed
Soyeon Bak <bluewaters1104@gmail.com>
2023-08-24 22:10:05 +0900

    0824 다이얼로그 수정 완료

--- Main/class_file/class_warning_msg.py
+++ Main/class_file/class_warning_msg.py
@@ -2,6 +2,7 @@ from UI.warning_dialog import Ui_WarningDialog
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt
+import os
 import sys
 
 
@@ -18,14 +19,23 @@ class MsgBox(QDialog, Ui_WarningDialog):
         self.ok_btn.clicked.connect(self.close)
 
     def set_dialog_type(self, type="", msg=""):
-        img = None
+        import os
+
+        # 작업 디렉토리를 현재 스크립트의 디렉토리로 변경
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
         if type == 1:
-            img = '../../img/icon/check.png'
+            import os
+            relative_path = "../../img/icon/check.png"
+
         if type == 2:
-            img = '../../img/icon/password.png'
+            relative_path = '../../img/icon/password.png'
+            absolute_path = os.path.abspath(relative_path)
             msg = '얼굴인식이 어렵습니다. \n '\
                   '아이디와 비밀번호로 로그인 해 주세요!'
 
+        absolute_path = os.path.abspath(relative_path)
+
         self.warn_lab.setText(msg)
-        self.icon_lab.setPixmap(QPixmap(img))
+        self.icon_lab.setScaledContents(True)
+        self.icon_lab.setPixmap(QPixmap(absolute_path))
