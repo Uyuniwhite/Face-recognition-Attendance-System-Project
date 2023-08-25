58ff04171523b2fbb0299a35cec3c894f6126532
노우현 <sdew301@naver.com>
2023-08-25 12:26:08 +0900

    콤보박스에 넣을 부서목록, 부서 고르면 부서에 해당되는 사원목록 리스트로 만들어주기

--- Main/class_file/class_dbconnect.py
+++ Main/class_file/class_dbconnect.py
@@ -87,11 +87,25 @@ class DBconnect:
 
         return dept_list
 
-    #
+    # 선택한 부서별 사원만 리스트 담아서 리턴
+    def select_dept(self, dept):
+        empolyee_list = list()
+        c = self.start_conn()
+        empolyee_query = "select tb_user.user_name from tb_user join tb_dept on tb_user.dept_id = tb_dept.dept_id " \
+                         f"where tb_dept.dept_name = '{dept}'"
+        c.execute(empolyee_query)
+        datas = c.fetchall()
+        for data in datas:
+            empolyee_name = data[0]
+            empolyee_list.append(empolyee_name)
+
+        return empolyee_list
+
+
 
 if __name__ == '__main__':
     db_conn = DBconnect(controller=None)
-    a = db_conn.find_dept()
+    a = db_conn.select_dept('인사팀')
     print(a)
 
 
