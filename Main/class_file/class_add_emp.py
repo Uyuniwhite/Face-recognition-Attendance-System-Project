d444b9318b37874cb6eaadff383fc53003d6713c
Soyeon Bak <bluewaters1104@gmail.com>
2023-08-26 14:02:51 +0900

    0825 사원추가 메세지박스 수정완

--- Main/class_file/class_add_emp.py
+++ Main/class_file/class_add_emp.py
@@ -3,13 +3,14 @@ from Main.class_file.class_warning_msg import MsgBox
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtCore import pyqtSignal
 
+
 class AddEmpolyee(QWidget, Ui_AddEmployee):
     def __init__(self, controller):
         super().__init__()
         self.main = controller
-        self.init_UI() # UI init
-        self.init_func() # function init
-
+        self.init_UI()  # UI init
+        self.init_func()  # function init
+        self.msgbox = MsgBox()
 
     def init_UI(self):
         self.setupUi(self)
@@ -20,9 +21,9 @@ class AddEmpolyee(QWidget, Ui_AddEmployee):
 
     def init_func(self):
         # 버튼 클릭 이벤트
-        self.cancel_btn.clicked.connect(self.close) # 취소버튼
-        self.admit_btn.clicked.connect(self.clicked_add_empolyee_btn) # 등록완료 버튼
-        self.face_rec_btn.clicked.connect(self.clicked_face_rec_btn) # 얼굴인식 버튼
+        self.cancel_btn.clicked.connect(self.close)  # 취소버튼
+        self.admit_btn.clicked.connect(self.clicked_add_empolyee_btn)  # 등록완료 버튼
+        self.face_rec_btn.clicked.connect(self.clicked_face_rec_btn)  # 얼굴인식 버튼
 
     # def clicked_add_empolyee_btn(self):
     #     emp_name = self.name_lineedit.text()
@@ -90,7 +91,6 @@ class AddEmpolyee(QWidget, Ui_AddEmployee):
         result_pw = self.verify_pw(emp_pw, emp_re_pw)
         result_dept = self.convert_dept(emp_dept)
 
-        msgbox = MsgBox() # 메시지박스 객체 생성
         message = ''
         if result_name == 0:
             message = "이름을 입력하지 않았거나 공백이 존재합니다!"
@@ -116,28 +116,28 @@ class AddEmpolyee(QWidget, Ui_AddEmployee):
                     elif self.face_regist:
                         message = "신규 사원 등록이 완료되었습니다!"
 
-                        msgbox.set_dialog_type(msg=message, img='check')
-                        msgbox.exec_()
+                        self.msgbox.set_dialog_type(msg=message, img='check')
+                        self.msgbox.exec_()
                         return self.close()
-
-        msgbox.set_dialog_type(msg=message)
-        msgbox.exec_()
+        if len(message) > 0:
+            self.msgbox.set_dialog_type(msg=message)
+            self.msgbox.exec_()
 
     # 이름 검증
     def verify_name(self, name):
         if len(name) == 0 or ' ' in name:
-            return 0 # 이름 미입력 또는 스페이스 입력
+            return 0  # 이름 미입력 또는 스페이스 입력
         elif len(name) > 8:
-            return 1 # 이름 8글자 초과
+            return 1  # 이름 8글자 초과
         else:
             return 2
 
     # 아이디 검증
     def verify_id(self, id_):
         if len(id_) == 0 or ' ' in id_:
-            return 0 # 공백 유무
+            return 0  # 공백 유무
         elif len(id_) > 15:
-            return 1 # 15자 초과
+            return 1  # 15자 초과
         else:
             duple_result = self.main.dbconn.id_duple_check(id_)
             if duple_result:
@@ -148,15 +148,14 @@ class AddEmpolyee(QWidget, Ui_AddEmployee):
     # 패스워드 검증
     def verify_pw(self, pw, re_pw):
         if len(pw) == 0 or ' ' in pw:
-            return 0 # 패스워드 미입력 또는 스페이스 입력
-        elif len(pw) > 20 :
-            return 1 # 20자 초과
+            return 0  # 패스워드 미입력 또는 스페이스 입력
+        elif len(pw) > 20:
+            return 1  # 20자 초과
         else:
             if pw != re_pw:
-                return 2 # 패스워드 2차 입력 다름
+                return 2  # 패스워드 2차 입력 다름
             else:
                 return 3
-
 
     # 부서 텍스트를 부서 번호로 변경
     def convert_dept(self, dept):
@@ -165,22 +164,34 @@ class AddEmpolyee(QWidget, Ui_AddEmployee):
 
     # 얼굴 인식 관련
     def clicked_face_rec_btn(self):
-        newbie_id = self.user_id_lineedit.text()
-        msgbox = MsgBox()
+        newbie_id = self.user_id_lineedit.text()  # 유저 아이디
+        user_name = self.user_id_lineedit.text()  # 유저 이름
+
         message = str()
         id_duple = self.verify_id(newbie_id)
-        if id_duple == 0: # 아이디 미입력 또는 공백 존재
+
+        if id_duple == 0:  # 아이디 미입력 또는 공백 존재
             message = "ID를 입력하지 않았거나 공백이 있습니다!"
-        elif id_duple == 1: # id 글자 초과
+        elif id_duple == 1:  # id 글자 초과
             message = "ID가 15자를 초과합니다!"
-        elif id_duple == 2: # id 검증 통과
+        elif id_duple == 2:  # id 검증 통과
             self.main.face_cap.SetName.emit(newbie_id)
-            self.main.face_cap.show()
+            if self.capture_user_face_msgbox(user_name):
+                self.main.face_cap.show()  # 얼굴 확인 페이지 이동
             return
-        elif id_duple == 3: # id 중복
+        elif id_duple == 3:  # id 중복
             message = "중복된 ID가 존재합니다!"
-        msgbox.set_dialog_type(msg=message)
-        msgbox.exec_()
+        self.msgbox.set_dialog_type(msg=message)
+        self.msgbox.exec_()
+
+    def capture_user_face_msgbox(self, user_name):
+        msg = f'{user_name}님 얼굴인식을 진행하시겠습니까?'
+        self.msgbox.set_dialog_type(msg=msg, img='camera', type=4)
+        self.msgbox.exec_()
+
+        if self.msgbox.result() == 1:
+            return True
+        return False
 
     # 콤보박스 데이터 넣기
     def set_data_cb(self):
