e3bdcae782baf86c1962fd70e59d8f0e566e7d5d
노우현 <sdew301@naver.com>
2023-08-29 14:04:26 +0900

    로딩창 스레드 구현 / 창 안닫힘

--- Main/class_file/class_openingload.py
+++ Main/class_file/class_openingload.py
@@ -1,7 +1,17 @@
 from Main.UI.Opening import Ui_Opening
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtCore import QThread, pyqtSignal
+
+
 class OpeningLoading(QWidget, Ui_Opening):
-    def __init__(self):
+    Shown = pyqtSignal()
+
+    def __init__(self, controller=None):
         super().__init__()
         self.setupUi(self)
+
+        self.controller = controller
+        self.Shown.connect(self.open_event)
+
+    def open_event(self):
+        self.close()
